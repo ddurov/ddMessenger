0bc7867a0aaf43a2ffd52533695289bb8828c0b7
ddurov <danyadurovnn@gmail.com>
2023-10-19 21:14:30 +0300

    обновление метода перевода ошибок на человеко-читаемые, изменён родительский класс

--- messager/src/main/java/com/ddprojects/messager/service/api/APIException.java
+++ messager/src/main/java/com/ddprojects/messager/service/api/APIException.java
@@ -3,7 +3,7 @@ package com.ddprojects.messager.service.api;
 import com.ddprojects.messager.R;
 import com.ddprojects.messager.service.fakeContext;
 
-public class APIException extends Throwable {
+public class APIException extends Exception {
     private final int code;
     public APIException(String errorMessage, int errorCode) {
         super(errorMessage);
@@ -14,53 +14,46 @@ public class APIException extends Throwable {
         return code;
     }
 
-    public static String translate(String method, String errorMessage) {
-        String humanReadableError = "API error: " + errorMessage;
-        switch (method) {
-            case "updates":
-                switch (errorMessage) {
-                    case "current entity 'update by product' not found":
-                        humanReadableError = fakeContext.getInstance().
-                                getString(R.string.error_update_item_by_product_not_found);
-                        break;
-                    case "current entity 'updates by product' not found":
-                        humanReadableError = fakeContext.getInstance().
-                                getString(R.string.error_update_items_by_product_not_found);
-                        break;
-                }
-                break;
-            case "email":
-                switch (errorMessage) {
-                    case "parameter 'code' are invalid":
-                        humanReadableError = fakeContext.getInstance().
-                                getString(R.string.error_email_code_invalid);
-                        break;
-                    case "parameter 'hash' are invalid":
-                        humanReadableError = fakeContext.getInstance().
-                                getString(R.string.error_developer_eblan);
-                        break;
-                }
-            case "user":
-                switch (errorMessage) {
-                    case "current entity 'account by login' not found":
-                        humanReadableError = fakeContext.getInstance().
-                                getString(R.string.error_welcome_account_login_not_found);
-                        break;
-                    case "current entity 'account by login' are exists":
-                        humanReadableError = fakeContext.getInstance().
-                                getString(R.string.error_welcome_account_login_are_exists);
-                        break;
-                    case "current entity 'account by username' are exists":
-                        humanReadableError = fakeContext.getInstance().
-                                getString(R.string.error_welcome_account_username_are_exists);
-                        break;
-                    case "parameter 'password' are invalid":
-                        humanReadableError = fakeContext.getInstance().
-                                getString(R.string.error_welcome_account_password_invalid);
-                }
-                break;
+    public static String translate(String errorMessage) {
+        String humanReadableError;
 
+        switch (errorMessage) {
+            case "current entity 'update by product' not found":
+                humanReadableError = fakeContext.getInstance().
+                        getString(R.string.error_update_item_by_product_not_found);
+                break;
+            case "current entity 'updates by product' not found":
+                humanReadableError = fakeContext.getInstance().
+                        getString(R.string.error_update_items_by_product_not_found);
+                break;
+            case "parameter 'code' are invalid":
+                humanReadableError = fakeContext.getInstance().
+                        getString(R.string.error_email_code_invalid);
+                break;
+            case "parameter 'hash' are invalid":
+                humanReadableError = fakeContext.getInstance().
+                        getString(R.string.error_developer_eblan);
+                break;
+            case "current entity 'account by login' not found":
+                humanReadableError = fakeContext.getInstance().
+                        getString(R.string.error_welcome_account_login_not_found);
+                break;
+            case "current entity 'account by login' are exists":
+                humanReadableError = fakeContext.getInstance().
+                        getString(R.string.error_welcome_account_login_are_exists);
+                break;
+            case "current entity 'account by username' are exists":
+                humanReadableError = fakeContext.getInstance().
+                        getString(R.string.error_welcome_account_username_are_exists);
+                break;
+            case "parameter 'password' are invalid":
+                humanReadableError = fakeContext.getInstance().
+                        getString(R.string.error_welcome_account_password_invalid);
+                break;
+            default:
+                humanReadableError = "API error: " + errorMessage;
         }
+
         return humanReadableError;
     }
 }

528c376cae44ece3d5f0608591f4e4609af8af43
ddurov <danyadurovnn@gmail.com>
2023-11-05 17:25:20 +0300

    обновлён сплешскрин для правильной обработки обновлений/переходов в активити

--- messager/src/main/java/com/ddprojects/messager/initialActivity.java
+++ messager/src/main/java/com/ddprojects/messager/initialActivity.java
@@ -1,31 +1,37 @@
 package com.ddprojects.messager;
 
+import static com.ddprojects.messager.service.api.APIRequester.executeApiMethodAsync;
 import static com.ddprojects.messager.service.api.APIRequester.executeApiMethodSync;
-import static com.ddprojects.messager.service.api.APIRequester.setupApiClient;
-import static com.ddprojects.messager.service.globals.PDDEditor;
+import static com.ddprojects.messager.service.fakeContext.liveData;
+import static com.ddprojects.messager.service.fakeContext.persistentDataOnDisk;
+import static com.ddprojects.messager.service.globals.appInitVars;
 import static com.ddprojects.messager.service.globals.hasInternetConnection;
-import static com.ddprojects.messager.service.globals.liveData;
-import static com.ddprojects.messager.service.globals.writeMessageInLogCat;
-import static com.ddprojects.messager.service.globals.persistentDataOnDisk;
 import static com.ddprojects.messager.service.globals.showToastMessage;
 import static com.ddprojects.messager.service.globals.writeErrorInLog;
+import static com.ddprojects.messager.service.globals.writeMessageInLogCat;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 
-import com.ddprojects.messager.service.api.APIException;
+import com.ddprojects.messager.models.SuccessResponse;
 import com.ddprojects.messager.models.Update;
+import com.ddprojects.messager.models.User;
+import com.ddprojects.messager.service.api.APIException;
+import com.ddprojects.messager.service.api.APIRequester;
+import com.ddprojects.messager.service.appService;
 import com.ddprojects.messager.service.fakeContext;
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Objects;
+
+import okhttp3.Response;
 
 public class initialActivity extends AppCompatActivity {
 
@@ -34,9 +40,6 @@ public class initialActivity extends AppCompatActivity {
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
 
-        persistentDataOnDisk = getSharedPreferences("data", Context.MODE_PRIVATE);
-        PDDEditor = persistentDataOnDisk.edit();
-
         if (new File(this.getDataDir(), "log.txt").delete()) writeMessageInLogCat("Logs deleted");
 
         if (!hasInternetConnection()) {
@@ -44,18 +47,13 @@ public class initialActivity extends AppCompatActivity {
             finish();
         }
 
-        setupApiClient();
-
-        Hashtable<String, String> updatesGetParams = new Hashtable<>();
-        updatesGetParams.put("product", "messager");
+        appInitVars();
 
         new Thread(() -> {
-            Update newUpdate = new Update(
-                    BuildConfig.VERSION_NAME,
-                    "No changes are applied"
-            );
-
             try {
+                Hashtable<String, String> updatesGetParams = new Hashtable<>();
+                updatesGetParams.put("product", "messager");
+
                 Update response = new Gson().fromJson(executeApiMethodSync(
                         "get",
                         "general",
@@ -64,12 +62,14 @@ public class initialActivity extends AppCompatActivity {
                         updatesGetParams
                 ).getBody(), Update.class);
 
-                newUpdate.setVersion(response.getVersion());
-                newUpdate.setDescription(response.getDescription());
-
-                liveData.put("update", newUpdate);
+                if (!Objects.equals(response.getVersion(), BuildConfig.VERSION_NAME)) {
+                    liveData.put("update", new Update(
+                            response.getVersion(),
+                            response.getDescription()
+                    ));
+                }
             } catch (APIException APIex) {
-                showToastMessage(
+                if (APIex.getCode() != 404) showToastMessage(
                         APIException.translate(APIex.getMessage()),
                         false
                 );
@@ -79,40 +79,69 @@ public class initialActivity extends AppCompatActivity {
                         fakeContext.getInstance().getString(R.string.error_request_failed),
                         false
                 );
-            } finally {
-                liveData.put("update", newUpdate);
             }
         }).start();
 
         splashScreen.setKeepOnScreenCondition(() -> {
-            if (liveData.containsKey("update")) {
+            if (liveData.contains("update")) {
                 Update update = (Update) liveData.get("update");
 
-                if (!Objects.equals(
-                        Objects.requireNonNull(update).getVersion(),
-                        BuildConfig.VERSION_NAME
-                )) {
-                    Intent intent = new Intent(this, updateActivity.class);
-                    intent.putExtra("newVersion", update.getVersion());
-                    intent.putExtra("description", update.getDescription());
-                    startActivity(intent);
-                    finish();
-                    return false;
-                }
+                Intent intent = new Intent(this, updateActivity.class);
+                intent.putExtra("newVersion", update.getVersion());
+                intent.putExtra("description", update.getDescription());
+                startActivity(intent);
+                finish();
 
+                liveData.remove("update");
+            } else {
                 if (
                         persistentDataOnDisk.contains("sessionId") &&
                                 persistentDataOnDisk.contains("token")
                 ) {
-                    startActivity(new Intent(this, dialogsActivity.class));
-                    finish();
+                    executeApiMethodAsync(
+                            "get",
+                            "product",
+                            "user",
+                            "get",
+                            new Hashtable<>(),
+                            new APIRequester.Callback() {
+                                @Override
+                                public void onFailure(Exception exception) {
+                                    if (exception instanceof APIException) {
+                                        showToastMessage(
+                                                APIException.translate(exception.getMessage()),
+                                                false
+                                        );
+                                    } else {
+                                        writeErrorInLog(exception);
+                                        showToastMessage(
+                                                getString(R.string.error_request_failed),
+                                                false
+                                        );
+                                    }
+                                }
+
+                                @Override
+                                public void onSuccess(Response response) throws IOException {
+                                    JsonElement userJson = new Gson().fromJson(
+                                            response.body().string(),
+                                            SuccessResponse.class
+                                    ).getBody();
+                                    liveData.put("user", new Gson().fromJson(userJson, User.class));
+
+                                    startService(new Intent(initialActivity.this, appService.class));
+                                    startActivity(new Intent(initialActivity.this, dialogsActivity.class));
+                                    finish();
+                                }
+                            }
+                    );
+                    return false;
                 } else {
                     startActivity(new Intent(this, welcomeActivity.class));
                     finish();
                 }
-                return false;
             }
-            return true;
+            return false;
         });
     }
 }

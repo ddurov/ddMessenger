7b5ad7b43cf2847b28d2516230d1b81aa2e20713
ddurov <danyadurovnn@gmail.com>
2023-12-31 16:40:14 +0300

    удаление точек инициализации объектов

--- messager/src/main/java/com/ddprojects/messager/service/fakeContext.java
+++ messager/src/main/java/com/ddprojects/messager/service/fakeContext.java
@@ -1,19 +1,12 @@
 package com.ddprojects.messager.service;
 
 import android.app.Application;
-import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Looper;
-
-import java.util.Hashtable;
 
 public class fakeContext extends Application {
     private static fakeContext instance;
     private static Handler mainThreadHandler;
-    public static observableHashtable<Object, Object> liveData;
-    public static Hashtable<String, String> APIEndPoints;
-    public static SharedPreferences persistentDataOnDisk;
-    public static SharedPreferences.Editor PDDEditor;
 
     public fakeContext() {
         instance = this;

78b5a3b323ac5b2405d127653927c4135d97f530
ddurov <danyadurovnn@gmail.com>
2023-12-31 16:36:08 +0300

    переход с синхронных запросов на асинхронные

--- messager/src/main/java/com/ddprojects/messager/service/appService.java
+++ messager/src/main/java/com/ddprojects/messager/service/appService.java
@@ -1,48 +1,64 @@
 package com.ddprojects.messager.service;
 
-import static com.ddprojects.messager.service.api.APIRequester.executeApiMethodSync;
-import static com.ddprojects.messager.service.globals.appInitVars;
-import static com.ddprojects.messager.service.globals.hasInternetConnection;
-import static com.ddprojects.messager.service.globals.showToastMessage;
+import static com.ddprojects.messager.service.api.APIRequester.executeApiMethod;
+import static com.ddprojects.messager.service.api.APIRequester.setupApiClient;
+import static com.ddprojects.messager.service.globals.cachedData;
+import static com.ddprojects.messager.service.globals.dialogs;
 import static com.ddprojects.messager.service.globals.writeErrorInLog;
 
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-import com.ddprojects.messager.R;
 import com.ddprojects.messager.models.Message;
+import com.ddprojects.messager.models.SuccessResponse;
 import com.ddprojects.messager.service.api.APIException;
+import com.ddprojects.messager.service.api.APIRequester;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Objects;
 
 public class appService extends Service {
     public void onCreate() {
         super.onCreate();
-        appInitVars();
+        setupApiClient();
+        cachedData.setOnEventListener(cacheService::updateInstance);
+        dialogs.setOnEventListener(table -> cachedData.put("dialogs", table));
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        new Thread(() -> {
-            while (true) {
-                if (hasInternetConnection()) {
-                    Hashtable<String, String> listenParams = new Hashtable<>();
-                    listenParams.put("timeout", "25");
+        update();
+        return Service.START_STICKY;
+    }
 
-                    try {
-                        JsonArray dataArray = executeApiMethodSync(
-                                "get",
-                                "product",
-                                "longpoll",
-                                "listen",
-                                listenParams
-                        ).getBody().getAsJsonArray();
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void update() {
+        Hashtable<String, String> listenParams = new Hashtable<>();
+        listenParams.put("timeout", "25");
+
+        executeApiMethod(
+                "get",
+                "product",
+                "longpoll",
+                "listen",
+                listenParams,
+                new APIRequester.Callback() {
+                    @Override
+                    public void onFailure(Exception exception) {
+                        if (!(exception instanceof APIException)) writeErrorInLog(exception);
+                    }
+
+                    @Override
+                    public void onSuccess(SuccessResponse response) {
+                        JsonArray dataArray = response.getBody().getAsJsonArray();
 
                         if (!dataArray.isEmpty()) {
                             for (JsonElement data : dataArray) {
@@ -58,35 +74,17 @@ public class appService extends Service {
                                                     messageData.get("id").getAsInt(),
                                                     messageData.get("senderAId").getAsInt(),
                                                     messageData.get("peerAId").getAsInt(),
-                                                    messageData.get("message").getAsString(),
-                                                    messageData.get("messageDate").getAsInt()
+                                                    messageData.get("text").getAsString(),
+                                                    messageData.get("time").getAsInt()
                                             )
                                     });
                                 }
                             }
                         }
-                    } catch (APIException APIEx) {
-                        showToastMessage(
-                                APIException.translate(APIEx.getMessage()),
-                                false
-                        );
-                    } catch (IOException IOEx) {
-                        writeErrorInLog(IOEx);
-                        showToastMessage(
-                                fakeContext.getInstance().getString(R.string.error_request_failed),
-                                false
-                        );
+
+                        update();
                     }
                 }
-            }
-        }).start();
-
-        return Service.START_STICKY;
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        );
     }
 }
